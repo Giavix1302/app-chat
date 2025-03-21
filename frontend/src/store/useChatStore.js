a796abf36741073912829ef7a938c2051de4548a
LeThiBaoUyen <lebaouyen17092004@gmail.com>
2025-03-21 07:50:35 +0700

    feat: add chat components and state management for user interactions

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+import toast from "daisyui/components/toast";
+import { axiosInstance } from "../lib/axios";
+import { User } from "lucide-react";
+
+export const useChatStore = create((set) => ({
+  massages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const response = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ massages: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
+}));
